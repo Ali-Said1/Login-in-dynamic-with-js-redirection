133f27fdbb76bbac0b86aa2c18a068bc805d5a1f
Ali-Said1 <85112596+Ali-Said1@users.noreply.github.com>
2021-07-26 13:50:26 +0200

    Added Labels, connected the <a> with password length

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-const submit = document.getElementById('submit');
-const un = document.getElementById('un');
-const pw = document.getElementById('pw');
-const link = document.getElementById('submitLink');
-
+const submit = document.getElementById("submit");
+const un = document.getElementById("un");
+const pw = document.getElementById("pw");
+const link = document.getElementById("submitLink");
 
 un.addEventListener("input", (unvalue) => {
-    const unValue = unvalue.currentTarget.value;
-    if(unValue === ""){
-        pw.disabled = true;
-    }else{
-        pw.disabled= false
-    }
+  const unValue = unvalue.currentTarget.value;
+  if (unValue === "") {
+    pw.disabled = true;
+  } else {
+    pw.disabled = false;
+  }
 });
 
 pw.addEventListener("input", (pwvalue) => {
-    const pwValue = pwvalue.currentTarget.value;
-    if(pwValue === ""){
-        submit.disabled = true;
-        link.style.visibility= "hidden";
-        submit.style.color= "grey";
-    }else{
-        submit.disabled = false;
-        submit.style.color= "white";
-        link.style.visibility= "visible";
-    }
+  const pwValue = pwvalue.currentTarget.value;
+  if (pwValue.length < 4) {
+    submit.disabled = true;
+    link.style.visibility = "hidden";
+    submit.style.color = "grey";
+  } else {
+    submit.disabled = false;
+    submit.style.color = "white";
+    link.style.visibility = "visible";
+  }
 });
-
-
-//function linkclick(){
-//    document.write("<h1 style=\"color: red; position: absolute; top: 50%; left: 50%;   transform: translate(-50%, -50%);\" >Hello</h1>")
-//}
